6505973c174687d36d8fccf083b020574c7050d7
GetieBalew24 <getiebalew2020@gmail.com>
2022-08-23 09:03:01 -0700

    insert at the begin

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,34 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-	number = abs(number) % 10
-	print(number, end="")
-	return number
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_node - inserts node in sorted list
+ * @head: address of head pointer
+ * @number: number to insert
+ * Return: inserted node
+ */ 
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	if (!node || new->n < node->n)
+	{
+		new->next = node;
+		return (*head = new);
+	}
+	while (node)
+	{
+		if (!node->next || new->n < node->next->n)
+		{
+			new->next = node->next;
+			node->next = new;
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
