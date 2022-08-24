259f3ced85906584546197024fe6985fe3ad0eaa
GetieBalew24 <getiebalew2020@gmail.com>
2022-08-24 04:46:28 -0700

    Added scripts that trims string edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First = word[:3]
+Last = word[-2:]
+Middle = word[1:-1]
+print(f"First 3 letters: {First}")
+print(f"Last 2 letters: {Last}")
+print(f"Middle word: {Middle}")
