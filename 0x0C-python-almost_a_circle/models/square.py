d4a1bdd56b9f91a467c68caecefd949f375f6bab
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-30 12:42:09 -0700

    update class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,6 +25,7 @@ class Square(Rectangle):
     def size(self):
         """Get/set the size of the Square."""
         return self.width
+
     @size.setter
     def size(self, value):
         self.width = value
