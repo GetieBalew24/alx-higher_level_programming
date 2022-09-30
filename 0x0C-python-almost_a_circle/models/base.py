69d07d86217d46861fddfb9190bd6ab0468594c3
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-30 14:29:44 -0700

    Update the class Base by adding the class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,3 +65,20 @@ class Base:
         # update temp with obj func update()
         temp.update(**dictionary)
         return temp
+    
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances """
+        res = []
+        with open(cls.__name__ + ".json", mode="r") as read_file:
+            text = read_file.read()
+        # Converting str to list
+        text = cls.from_json_string(text)
+        for item in text:
+            # Formatting dicts into str format
+            if type(item) == dict:
+                res.append(cls.create(**item))
+            else:
+                res.append(item)
+        return res
+   
